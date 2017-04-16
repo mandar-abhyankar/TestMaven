095c2ea214f06d2e08c8318eae33799ccbf3cdd3
Mandar Abhyankar <mandar.abhyankar@live.com>
2017-04-16 21:23:12 +0530

    Added src\main\java\SampleClass.java file for commit to test pmd plugin in eclipse

---
+++ src/main/java/SampleClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Mandar Abhyankar on 4/16/2017.
+ */
+public class SampleClass {
+
+    private String Mandar;
+    public void sampleMethod(){
+
+        System.out.println("Here");
+    }
+
+
+}
+
+
+
